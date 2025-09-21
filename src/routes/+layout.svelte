77f27e268ca76cf216815379bdb972bde1a6fa26
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-09-21 15:00:32 +0200

    Remove extra metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,8 @@
 <script>
 	import '../app.css';
-	import { pageTitle, setPageTitle } from '$lib/stores/page-title.js';
 	import { onMount } from 'svelte';
 
 	let { children } = $props();
-	
-	// Set default title on mount
-	onMount(() => {
-		setPageTitle();
-	});
 </script>
-
-<svelte:head>
-	<title>{$pageTitle}</title>
-</svelte:head>
 
 {@render children()}
